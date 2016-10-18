61013cd5b73d12f1b69d9a359dd5c89a7f9253a4
BiaoLiu <452381072@qq.com>
2016-10-18 09:37:12 +0800

    合并

--- boai/apps/wechat/urls.py
+++ boai/apps/wechat/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 
-from .views import views as boai_wechat_views
-from .views import views, user
+from .views import main as boai_wechat_views
+from .views import main, user
 
 urlpatterns = [
     # url(r'^$', boai_wechat_views.index),
     url(r'^set_menu$', boai_wechat_views.create_menu),
 
-    url(r'^$', views.main, name='main'),
+    url(r'^$', main.main, name='main'),
 
     url(r'^register/$', user.Register.as_view(), name='register'),
 

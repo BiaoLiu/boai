9326dd9ba3fc9cf52aaea56687a316c9f740f51a
BiaoLiu <452381072@qq.com>
2016-11-07 16:21:51 +0800

    后台管理

--- boai/apps/boai_admin/admin.py
+++ boai/apps/boai_admin/admin.py
@@ -1,22 +1,70 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.core.urlresolvers import reverse
 from django.utils.html import format_html
+from django.utils.translation import ugettext_lazy as _
 
+from boai.apps.boai_admin.forms import BoaiUserChangeForm, BoaiUserCreationForm
 from boai.apps.boai_model import models
 
 
 # Register your models here.
 
-@admin.register(models.AuthUser)
-class UserAdmin(admin.ModelAdmin):
-    list_per_page = 15
-    list_display = ['id', 'mobile', 'realname', 'email', 'date_joined', 'is_active']
-    list_display_links = ['id', 'mobile', 'realname']
+# @admin.register(models.AuthUser)
+# class UserAdmin(admin.ModelAdmin):
+#     list_per_page = 15
+#     list_display = ['id', 'mobile', 'email', 'date_joined', 'is_active']
+#     list_display_links = ['id', 'mobile']
+#     fields = ['username', 'mobile', 'nicknickname', 'realnickname', 'idcart', 'email', 'date_joined', 'is_active']
+#
+#     form = UserForm
 
-    def realname(self, instance):
-        return instance.profile.realname
 
-    realname.short_description = '姓名'
+
+
+class BoaiUserAdmin(UserAdmin):
+    def __init__(self, *args, **kwargs):
+        super(BoaiUserAdmin, self).__init__(*args, **kwargs)
+        self.list_display = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
+        self.search_fields = ('username', 'email', 'nickname')
+
+        self.form = BoaiUserChangeForm  # 编辑用户表单
+        self.add_form = BoaiUserCreationForm  # 添加用户表单
+
+    def changelist_view(self, request, extra_context=None):
+        # 这个方法在源码的admin/options.py文件的ModelAdmin这个类中定义，我们要重新定义它，以达到不同权限的用户，返回的表单内容不同
+
+        self.fieldsets = ((None, {'fields': ('username', 'password',)}),
+                          (_('Personal info'), {'fields': ('realname', 'mobile', 'idcart', 'email')}),
+                          (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups')}),
+                          (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+                          )
+        self.add_fieldsets = ((None, {'classes': ('wide',),
+                                      'fields': (
+                                          'username', 'mobile', 'password1', 'password2', 'realname', 'idcart', 'email',
+                                          'is_active',
+                                          'is_staff', 'is_superuser', 'groups'),
+                                      }),
+                              )
+
+        return super(BoaiUserAdmin, self).changelist_view(request, extra_context)
+
+    def save_model(self, request, obj, form, change):
+        realname = form.cleaned_data.get('realname')
+        idcart = form.cleaned_data.get('idcart')
+        obj.save()
+
+        if not change:
+            user_profile = models.AppUserProfile(user_id=obj.id)
+        else:
+            user_profile = form.instance.profile
+
+        user_profile.realname = realname
+        user_profile.idcart = idcart
+        user_profile.save()
+
+
+admin.site.register(models.AuthUser, BoaiUserAdmin)
 
 
 class SalesorderitemInline(admin.StackedInline):
@@ -33,7 +81,7 @@ class SalesorderitemInline(admin.StackedInline):
 
     # ('Advanced options', {
     #     'classes': ('collapse',),
-    #     'fields': ('enable_comments', 'registration_required', 'template_name')
+    #     'fields': ('enable_comments', 'registration_required', 'template_nickname')
     # }),
 
     def has_delete_permission(self, request, obj=None):
@@ -54,7 +102,7 @@ class SalesorderAdmin(admin.ModelAdmin):
     def user_link(self, instance):
         realname = instance.user.profile.realname
         url = ''
-        return format_html("""<a href="{0}">{1}</a>""", url, realname)
+        return format_html('<a href="{0}">{1}</a>', url, realname)
 
     user_link.short_description = "用户"
     # 显示HTML
--- boai/apps/boai_admin/forms.py
+++ boai/apps/boai_admin/forms.py
@@ -1,1 +1,49 @@
 # coding: utf-8
+from django import forms
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+from boai.apps.boai_model.models import AuthUser
+
+
+class UserForm(forms.ModelForm):
+    realname = forms.CharField(max_length=20, required=False)
+    idcart = forms.CharField(max_length=20, required=False)
+
+    # social_city = forms.CharField(max_length=20, blank=True, null=True)
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs['instance']
+
+        kwargs['initial'] = {
+            'realname': user.profile.realname,
+            'idcart': user.profile.idcart
+        }
+
+        super(UserForm, self).__init__(**kwargs)
+
+    class Meta:
+        model = AuthUser
+        exclude = ('',)
+
+
+class BoaiUserCreationForm(UserCreationForm):
+    realname = forms.CharField(label='姓名', max_length=20, required=False)
+    idcart = forms.CharField(label='身份证号码', max_length=20, required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(BoaiUserCreationForm, self).__init__(*args, **kwargs)
+        # self.fields['email'].required = True  # 为了让此字段在admin中为必选项，自定义一个form
+
+
+class BoaiUserChangeForm(UserChangeForm):
+    realname = forms.CharField(label='姓名', max_length=20, required=False)
+    idcart = forms.CharField(label='身份证号码', max_length=20, required=False)
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs['instance']
+        kwargs['initial'] = {
+            'realname': user.profile.realname,
+            'idcart': user.profile.idcart
+        }
+        super(BoaiUserChangeForm, self).__init__(*args, **kwargs)
+        self.fields['email'].required = True
